4435e743baa31f7c4b533b16706d3710151d34e5
Kiel Arneson <kiel.arneson@gmail.com>
2021-10-11 11:10:51 -0500

    Added login page to allow user to login to site

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import IndexProduct from "../views/IndexProduct.vue";
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -22,6 +23,7 @@ const routes = [
   },
   { path: "/products", name: "IndexProduct", component: IndexProduct },
   { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
 ];
 
 const router = new VueRouter({
